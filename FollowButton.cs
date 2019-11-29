d890a01d155c8942449d363cb8f316c2026bd5dd
yung-shuva <58325973+yung-shuva@users.noreply.github.com>
2019-11-29 22:36:27 +0300

    Add files via upload

---
+++ FollowButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FollowButton : MonoBehaviour
+{
+    private void Update()
+    {
+        var objects = GameObject.FindGameObjectsWithTag("Player");
+        if (!objects.Any())
+            return;
+
+        var target = objects.First();
+        if (target == null)
+            return;
+
+        var p1 = transform.position;
+        var p2 = target.transform.position;
+        var position = new Vector3(p2.x, p2.y, p2.z);
+        transform.LookAt(position);
+    }
+}
